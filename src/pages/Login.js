6eaec0eb583967f6c0d6d766860862ee0dbe8307
Daniel Oliveira <danoliveira865@gmail.com>
2022-12-14 23:14:18 -0300

    Cria page Login.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.validateForm());
+  };
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    const passwordLengthMin = 4;
+    const emailValidateRegex = /\S+@\S+\.\S+/;
+    const emailValidate = emailValidateRegex.test(email);
+    const passwordValidate = password.length > passwordLengthMin;
+    if ((emailValidate && passwordValidate) === true) {
+      this.setState({
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  };
+
+  pushHistory = () => {
+    const { history } = this.props;
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled, email, password } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            placeholder="senha"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </div>
+        <button
+          type="button"
+          onClick={ this.pushHistory }
+          disabled={ isDisabled }
+        >
+          Entrar
+
+        </button>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,4 +1,6 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { REQUEST_API, RESQUEST_SUCCESS_API } from '../actions';
+
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
@@ -8,6 +10,19 @@ const INITIAL_STATE = {
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case REQUEST_API: {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  case RESQUEST_SUCCESS_API: {
+    return {
+      ...state,
+      isLoading: false,
+
+    }
+  }
   default: return state;
   }
 };

69debbe61ab5b2de28047fc631f1293fdb223bf3
Daniel Oliveira <danoliveira865@gmail.com>
2023-01-16 16:01:31 -0300

    Requisito 2 finalizado. Cria header e exibe email do estado global.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
-        <h3 data-testid="email-field">email</h3>
+        <h3 data-testid="email-field">{email}</h3>
         <h3 data-testid="total-field">Despesa total: 0</h3>
         <h3 data-testid="header-currency-field">Moeda: BRL</h3>
       </div>
@@ -12,4 +15,12 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
